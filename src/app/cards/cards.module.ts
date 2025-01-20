8c8e1c7f55029185074cd4e8d3c4203c2d45b8e7
Yane Wanderley <yanewanderley@gmail.com>
2025-01-20 17:11:12 -0300

    39. Terceiro Caso de Uso com ::ng-deep e :host (Componentes Externos)

--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -4,16 +4,20 @@ import { CardComponent } from '../card/card.component';
 import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
 import { CardButtonComponent } from '../card-button/card-button.component';
 import { CardRoxoButtonComponent } from '../card-roxo-button/card-roxo-button.component';
-
+import { CardButtonCancelComponent } from '../card-button-cancel/card-button-cancel.component';
+import { MatSliderModule } from '@angular/material/slider';
+ 
 @NgModule({
   declarations: [
       CardComponent,
       CardRoxoComponent,
       CardButtonComponent,
-      CardRoxoButtonComponent
+      CardRoxoButtonComponent,
+      CardButtonCancelComponent
     ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSliderModule
   ],  
   exports: [
     CardComponent,
